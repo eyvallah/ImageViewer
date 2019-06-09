1db918f06722512f4a2e99a1bbbdc8d435648ad6
li <530057072@qq.com>
2019-06-09 18:37:09 +0800

    use swift package manager

---
+++ Tests/ImageViewerTests/ImageViewerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ImageViewer
+
+final class ImageViewerTests: XCTestCase {
+    func testExample() {
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
